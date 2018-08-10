7298a9540490d719d749ff74715f8e539bbbe7dd
Garry <ipanchenko@cuber.com.ua>
2018-08-10 18:03:00 +0300

    test

--- app/src/main/java/ua/com/cuber/ischooler/MainActivity.java
+++ app/src/main/java/ua/com/cuber/ischooler/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_about_school) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_about_school_info) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_about_school_rings) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_about_school_teachers) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_timetable) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_rating) {
 
         }
 
